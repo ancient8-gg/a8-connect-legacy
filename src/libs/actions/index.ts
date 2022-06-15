451122fe798b9180fdf169e8e2cc302c45ed648d
0xTraderTrou <khangtran.btc@gmail.com>
2022-06-16 01:25:46 +0700

    feat: export actions

--- src/libs/actions/index.ts
+++ src/libs/actions/index.ts
@@ -1,1 +1,3 @@
-export {};
+export { WalletAction } from "./wallet.action";
+export { AuthAction } from "./auth.action";
+export { UserAction } from "./user.action";
--- src/libs/actions/offchain.action.ts
+++ src/libs/actions/offchain.action.ts
@@ -41,24 +41,16 @@ export class OffChainAction {
   protected readonly storageProvider: StorageProvider;
 
   /**
-   * Constructor needs `NetworkType`, `Document`, `Storage` and `fetch` passing as parameters.
+   * Constructor needs `NetworkType` passing as parameter.
    * @param networkType
-   * @param document
-   * @param storage
-   * @param fetchInstance
    */
-  constructor(
-    networkType: NetworkType,
-    document: Document,
-    storage: Storage,
-    fetchInstance: typeof fetch
-  ) {
+  constructor(networkType: NetworkType) {
     // Initialize registry
     const registryProvider = RegistryProvider.getInstance();
     registryProvider.networkType = networkType;
-    registryProvider.document = document;
-    registryProvider.fetch = fetchInstance;
-    registryProvider.storage = storage;
+    registryProvider.document = (window as any).document;
+    registryProvider.fetch = (window as any).fetch;
+    registryProvider.storage = (window as any).localStorage;
 
     // Initialize provider
     this.authProvider = getAuthProvider();
