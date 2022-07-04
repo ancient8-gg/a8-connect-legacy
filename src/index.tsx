b0637eba3d81e7d04e5e2da711eba7740fb7465e
0xTraderTrou <khangtran.btc@gmail.com>
2022-07-04 11:27:32 +0700

    feat: update index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import { A8Connect } from "./lib.container";
 import { NetworkType } from "./libs/providers/registry.provider";
 import { ChainType } from "./libs/adapters";
 
-const a8Connect = new A8Connect("a8-connect");
-a8Connect
-  .init({
-    networkType: NetworkType.testnet,
-    chainType: ChainType.EVM,
-    onClose: () => {
-      a8Connect.closeModal();
-    },
-  })
-  .then(() => {
-    a8Connect.openModal();
-  });
+if (document) {
+  document.onload = () => {
+    const a8Connect = new A8Connect("a8-connect");
+    a8Connect
+      .init({
+        networkType: NetworkType.testnet,
+        chainType: ChainType.SOL,
+        onClose: () => {
+          a8Connect.closeModal();
+        },
+      })
+      .then(() => {
+        a8Connect.openModal();
+      });
 
-if (window) {
-  (window as any).a8Connect = a8Connect;
+    if (window) {
+      (window as any).a8Connect = a8Connect;
+    }
+  };
 }
