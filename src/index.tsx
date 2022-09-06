c125be18d2dcf92c5713546db86f8d0ff3842ce7
0xTraderTrou <khangtran.btc@gmail.com>
2022-09-06 17:34:45 +0700

    feat: enable dev server for sdk built-in flows

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { init, openModal } from "./browser";
-import RPCWalletAdapter from "./adapter";
 import { NetworkType } from "./libs/providers";
 import { ChainType } from "./libs/adapters";
 import { AppFlow } from "./components/router";
+import { RPCWalletAdapter } from "./adapter.cointainer";
 
 if (document) {
   document.onreadystatechange = function () {
@@ -13,9 +13,25 @@ if (document) {
         networkType: NetworkType.testnet,
         chainType: ChainType.SOL,
         initAppFlow: AppFlow.LOGIN_FLOW,
+        async onConnected(payload) {
+          if (payload.chainType === ChainType.SOL) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            window.rpcWallet = await RPCWalletAdapter.getSolanaWalletAdapter(
+              payload.walletName
+            );
+          }
+          if (payload.chainType === ChainType.EVM) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            window.rpcWallet = await RPCWalletAdapter.getEVMWalletAdapter(
+              payload.walletName,
+              payload.provider.injectedProvider
+            );
+          }
+        },
       }).then(() => {
         openModal();
-        console.log(RPCWalletAdapter);
       });
     }
   };
